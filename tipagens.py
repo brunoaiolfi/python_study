52d59886c44691d3c994570f242394467a840387
Bruno Aiolfi <bruno.aiolfi154@gmail.com>
2023-03-02 19:56:42 -0300

    Add files via upload

---
+++ tipagens.py
@@ -0,0 +1,16 @@
+from pickle import TRUE
+
+
+helloWorld = str
+helloWorld = 'Ola mundo'
+
+numeroInteiro = int
+numeroInteiro = 2
+
+numeroFloat = float
+numeroFloat = 0.5
+
+valorBooleano = bool
+valorBooleano = bool(numeroFloat > numeroInteiro)
+
+print(helloWorld, numeroInteiro, numeroFloat, valorBooleano)
