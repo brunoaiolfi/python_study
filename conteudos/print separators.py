23222c37c8d57eb4873cb027ff5171034d353881
Bruno Aiolfi <bruno.aiolfi154@gmail.com>
2023-03-02 20:05:52 -0300

    Add files via upload

---
+++ conteudos/print separators.py
@@ -0,0 +1,5 @@
+dia = '02'
+mes = '03'
+ano = '2023'
+
+print(dia, mes, ano, sep='/',end=';')
---
+++ conteudos/tipagens.py
@@ -0,0 +1,13 @@
+helloWorld = str
+helloWorld = 'Ola mundo'
+
+numeroInteiro = int
+numeroInteiro = 2
+
+numeroFloat = float
+numeroFloat = 0.5
+
+valorBooleano = bool
+valorBooleano = bool(numeroFloat > numeroInteiro)
+
+print(helloWorld, numeroInteiro, numeroFloat, valorBooleano)
